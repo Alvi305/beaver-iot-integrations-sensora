a478d32039469d30a11e4ba3255f2c719d5428e9
Alvi305 <alvirahman305@gmail.com>
2025-06-27 11:55:45 +0800

    feat: added custom integration

---
+++ integrations/sensora-integration/src/main/java/com/milesight/beaveriot/integrations/SensoraIntegrationBootstrap.java
@@ -0,0 +1,23 @@
+package com.milesight.beaveriot.integrations;
+
+import com.milesight.beaveriot.context.integration.bootstrap.IntegrationBootstrap;
+import com.milesight.beaveriot.context.integration.model.Integration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SensoraIntegrationBootstrap implements IntegrationBootstrap {
+    @Override
+    public void onPrepared(Integration integration) {
+        // do nothing
+    }
+
+    @Override
+    public void onStarted(Integration integrationConfig) {
+        System.out.println("Hello, world!");
+    }
+
+    @Override
+    public void onDestroy(Integration integration) {
+        // do nothing
+    }
+}
---
+++ integrations/sensora-integration/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.milesight.beaveriot.integrations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
